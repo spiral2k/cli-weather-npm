7b838fa96ed7783c5f17b210fdbac45ba6f9400b
spiral2k <Hhbadmin@gmail.com>
2016-03-11 15:15:47 +0200

    promise things

--- exe-promise.js
+++ exe-promise.js
@@ -1,9 +1,53 @@
-function doWork(data, callback){
+//function doWork(data, callback){
+//    callback('done');
+//}
+//
+//function doWorkPromise(data){
+//    return new Promise(function(resolve, reject){
+//        reject('éverything NOT worked!');
+//        //reject({
+//        //    error:'somthing bad happend'
+//        //});
+//    });
+//}
+//
+//doWorkPromise('some data').then(function(data){
+//    console.log(data);
+//},function(error){
+//    console.log(error);
+//});
+var request = require('request');
 
+function getWeather(location){
 
+    return new Promise(function(resolve, reject){
+
+        location = encodeURIComponent(location);
+
+        if(!location){
+            return reject("No location provided.");
+        }
+
+        var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=metric&appid=44db6a862fba0b067b1930da0d769e98';
+
+        request({
+            url: url,
+            json: true
+        }, function(error, response, body){
+
+            if(error){
+                reject('Unable to fetch weather.');
+            }else {
+                resolve('It\'s ' + body.main.temp + ' in ' + body.name);
+            }
+
+        });
+    });
 }
 
-function doWorkPromise(data){
-    
+getWeather('new york').then(function(currentWeather){
+    console.log(currentWeather);
+},function(error){
+    console.log(error);
 
-}
+})
---
+++ exe-promise2.js
@@ -0,0 +1,31 @@
+//function doWork(fail){
+//    return new Promise(function(resolve, reject){
+//       setTimeout(function(){
+//            if(typeof fail === 'boolean' && fail === true){
+//                reject('eeeerrrooorrr');
+//            }else{
+//                resolve('yeeeeshhhh');
+//            }
+//        }, 1000) ;
+//    });
+//}
+//
+//doWork(true).then(function(message){
+//    console.log(message);
+//    return doWork(true);
+//}).then(function(message){
+//    console.log(message);
+//}).catch(function(error){
+//    console.log(error);
+//});
+
+
+function getLocation(location){
+    return new Promise(function(resolve, reject){
+        return
+    });
+}
+
+function getWeather(){
+
+}
